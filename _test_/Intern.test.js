7614f79767f36bc691d81d3e3d5cc5b1823a9694
Doonywoony <javismcarr@gmail.com>
2022-01-15 16:10:47 -0500

    Intern.test.js constructor file created

---
+++ _test_/Intern.test.js
@@ -0,0 +1,32 @@
+//Intern as a constructor
+
+const { expect } = require('@jest/globals');
+const Intern = require('../lib/Intern');
+
+//Intern as an object
+
+test('creating an intern object', () => {
+    const intern = new Intern('Javis', 118911, 'javis.carr@gmail.com', 'AAMU');
+
+    expect(intern.school).toEqual(expect.any(String));
+
+});
+
+//will retrieve school from getSchool()
+
+test('where is the intern  school', () => {
+    const intern = new Intern('Javis', 118911, 'javis.carr@gmail.com', 'AAMU');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+
+//will retrieve the role from getRole()
+
+test('what is the employee role', () => {
+    const intern = new Intern ('Javis', 118911, 'javis.carr@gmail.com', 'AAMU');
+
+    expect(intern.getRole()).toEqual("Intern");
+});
+
+    

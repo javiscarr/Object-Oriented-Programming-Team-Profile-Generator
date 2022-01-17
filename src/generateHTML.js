6f5a8b1257e05e8e505e18c15381c1c38b11a1ee
Doonywoony <javismcarr@gmail.com>
2022-01-17 13:03:02 -0500

    created intern card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -49,4 +49,26 @@ const generateEngineer = function (engineer) {
 }
 
 
+//Intern card is created
 
+const generateIntern = function (intern) {
+    
+    return `
+    
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Manager</h4><i class="bi bi-briefcase"></i>
+            </div>
+
+            <div class="card-body">
+            <p class="id">ID: ${intern.id}</p>
+            <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+            <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+
+    `;
+}
